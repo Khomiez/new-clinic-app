a4d34a253ddcf0db31038b127ad2cabe3cc23595
judzuii <khomchan.wi@kkumail.com>
2025-05-16 14:07:50 +0700

    delete index

--- index.ts
+++
@@ -1,7 +0,0 @@
-export * from './.git';
-export * from './.next';
-export * from './node_modules';
-export * from './public';
-export * from './src';
-export * from './next-env.d';
-export * from './next.config';
