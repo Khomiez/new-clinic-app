029e0875f6246240a3b5e6c65783c92c39b0936f
judzuii <khomchan.wi@kkumail.com>
2025-05-24 18:17:29 +0700

    update index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,7 +3,6 @@ export {default as Card} from './Card';
 export {default as DocumentUpload} from './DocumentUpload';
 export {default as ErrorScreen} from './ErrorScreen';
 export {default as FileUploader} from './FileUploader';
-export {default as FileList} from './FileList';
 export {default as HistoryRecord} from './HistoryRecord';
 export {default as LoadingScreen} from './LoadingScreen';
 export {default as MedicalHistorySection} from './MedicalHistorySection';
